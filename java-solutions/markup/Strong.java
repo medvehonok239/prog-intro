cbf8433d8b4755187b1f82c6020d21aa5bd56036
medvehonok239 <90005242+medvehonok239@users.noreply.github.com>
2022-05-19 01:31:40 +0300

    Create Strong.java

---
+++ java-solutions/markup/Strong.java
@@ -0,0 +1,20 @@
+package markup;
+
+import java.util.List;
+
+public class Strong extends Symbol implements Type {
+
+    public Strong(List<Type> list1){
+        super(list1);
+    }
+
+    @Override
+    public void toMarkdown(StringBuilder sb) {
+        toMarkdown(sb,"__");
+    }
+
+    @Override
+    public void toBBCode(StringBuilder sb) {
+        toBBCode(sb,"[b]", "[/b]");
+    }
+}
