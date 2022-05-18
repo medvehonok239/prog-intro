355da000374c1de7cd185d1be05a6fe7b441a115
medvehonok239 <90005242+medvehonok239@users.noreply.github.com>
2022-05-19 01:27:50 +0300

    Create Add.java

---
+++ java-solutions/expression/Add.java
@@ -0,0 +1,15 @@
+package expression;
+
+class Add extends BinaryOperation {
+
+    public Add(Name name1, Name name2) {
+        super(name1, name2);
+        sign = '+';
+    }
+
+    @Override
+    public int count(int first, int second) {
+        return first + second;
+    }
+
+}
