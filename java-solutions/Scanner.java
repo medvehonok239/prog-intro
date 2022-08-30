947df4aa0bd189e9477105036e815c0898c05da1
Michael Kim <90005242+medvezhonokok@users.noreply.github.com>
2022-08-31 01:10:08 +0300

    Update Scanner.java

--- java-solutions/Scanner.java
+++ java-solutions/Scanner.java
@@ -61,10 +61,4 @@ public class Scanner {
     public void close() throws IOException {
         in.close();
     }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println(in.nextInt() + in.nextInt());
-    }
 }
